69f26c298a745b4c03b2a2a7647b8a5d792b2e22
zxreaper <863276843@qq.com>
2022-12-23 16:10:29 +0800

    KMP优化

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -14,9 +14,17 @@ int* getnext(string s)
     while(j < len - 1)
     {
         if((k == -1) || (s[j] == s[k]))
-        {
+         {
             j++, k++;
-            next[j] = k;
+            if(s[k] == s[j])
+            {
+                // kmp算法的优化
+                next[j] = next[k];
+            }
+            else
+            {
+                next[j] = k;
+            }
         }
         else
         {
@@ -29,6 +37,14 @@ int* getnext(string s)
 int KMP(string s, string t)
 {
     int* next = getnext(t);
+
+    cout << t << " : ";
+    for(int i = 0; i<t.size(); i++)
+    {
+        cout << next[i] << " ";
+    }
+    cout<<endl;
+
     unique_ptr<int> ptr(next);   // 相当于加上了delete操作
     int i = 0, j = 0, len_s = s.size(), len_t = t.size();
     while(i<len_s && j<len_t)
@@ -48,7 +64,7 @@ int KMP(string s, string t)
 int main()
 {
     string s = "ABCDCABDEFG";
-    string t = "ABD";
+    string t = "ABCABCAABCABCAA";
     
     int pos = KMP(s, t);  // 找到了返回其实位置，否则返回-1
     cout << pos << endl;
